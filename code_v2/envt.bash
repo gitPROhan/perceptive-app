7abf01d14bd72f98ac9e264b694f172bd7b6244a
Aanvik Bhatnagar <127507006+AanvikBh@users.noreply.github.com>
2024-03-15 14:31:05 +0530

    feature: added 'envt.bash'
    run 'bash envt.bash' from code_v2 and it will set up and run virtual envt in backend folder

---
+++ code_v2/envt.bash
@@ -0,0 +1,25 @@
+# sets up the virtual environment in backend folder
+
+cd ./backend
+
+
+python3 -m venv ./backend/environmentname
+source ./backend/environmentname/bin/activate
+
+pip install Flask
+pip install pymongo
+pip3 install numpy
+pip install opencv-python
+pip install Pillow
+pip install sk-video
+pip install matplotlib
+pip install --no-cache-dir torchvision
+pip install yacs
+pip3 install -U scikit-learn scipy matplotlib
+pip install scikit-image
+pip install Flask-Cors
+# if running on ubuntu use this command:
+# sudo apt-get install python3-opencv
+
+# if running on mac, use this command:
+brew install opencv
